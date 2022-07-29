fe55a6d46431cdd3b452ab81655a5f6554e09abf
Eric D'Alessandro <edalessandro22@jcu.edu>
2022-07-28 22:57:14 -0400

    Home page version 1.0 complete

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hejimedia
-Website for hejimedia.com. My personal photography portfolio.
+Website for hejimedia.co. My personal photography portfolio.
+
+http://hejimedia.co is hosted on an AWS Lightsail Ubuntu instance using Apache2. Currently hejimedia.co only has an http port open because the price is almost 10 times more for https ports.
---
+++ script.js
@@ -0,0 +1,20 @@
+/**
+ * Javascript functions for the hejimedia.co website.
+ * Author: Eric D'Alessandro - 07/28/2022
+ */
+
+// Ensure body has loaded before script can run.
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready()
+}
+
+// Global vars
+// Used for changing the color and underline of the nav item that is currently selected.
+var currentPageNavSelection
+
+// Do on page load.
+function ready() {
+
+}
